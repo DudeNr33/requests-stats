44c6a71586471efd8e12b1ed78ac3de47401b089
DudeNr33 <3929834+DudeNr33@users.noreply.github.com>
2026-02-21 20:49:27 +0100

    tests: update playwright tests

--- tests/adapters/test_playwright.py
+++ tests/adapters/test_playwright.py
@@ -4,11 +4,22 @@ from requests_stats.adapters.playwright import SyncRequestHandler
 from requests_stats.storage.in_memory_storage import InMemoryStorage
 
 
-def test_has_title(page: Page):
+def test_stores_unfiltered_requests(page: Page):
+    storage = InMemoryStorage()
+    handler = SyncRequestHandler(storage)
+    handler.register_on(page)
+    page.goto("https://automationintesting.online")
+    page.get_by_role(role="link", name="Admin", exact=True).click()
+    assert len(storage.recordings) > 0
+    assert any(x.path.startswith("/api") for x in storage.recordings)
+    assert any(x.path == "/" for x in storage.recordings)
+
+
+def test_stores_filtered_requests(page: Page):
     storage = InMemoryStorage()
     handler = SyncRequestHandler(storage, path_pattern=r"/api/.*")
     handler.register_on(page)
     page.goto("https://automationintesting.online")
     page.get_by_role(role="link", name="Admin", exact=True).click()
     assert len(storage.recordings) > 0
-    assert all(x.url.startswith("/api") for x in storage.recordings)
+    assert all(x.path.startswith("/api") for x in storage.recordings)
